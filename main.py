aaf364e4b1366a09297b25a02b3d96bc8617f56b
Ricardo Alberto González Cedeno <169106488+RickGZ007@users.noreply.github.com>
2026-03-20 00:20:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-#http://127.0.0.1:8000/
-@app.get("/")
+
+@app.get("/helloworld")
 async def root():
     return {"message": "Hello World"}
 
 
-#http://127.0.0.1:8000/teste1
-@app.get("/teste1")
+
+@app.get("/funcaoteste")
 async def funcao1():
     return {"teste": "Deu certo"}
